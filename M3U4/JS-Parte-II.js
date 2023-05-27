fa5ccaf2d40307ba332769daa7335d40fd94e09f
facundomaksud@gmail.com <facundomaksud@gmail.com>
2023-05-27 12:59:29 -0300

    ejercicios terminados

--- M3U4/JS-Parte-II.js
+++ M3U4/JS-Parte-II.js
@@ -22,43 +22,43 @@ function contarCaracteres() {
     contador.innerText = textarea.value.length
 }
 // Ejercicio 3
-// let alumnos = [
-//   {
-//     nombre: "Juan Gomez",
-//     nota: 7,
-//   },
-//   {
-//     nombre: "Pedro Rodriguez",
-//     nota: 4,
-//   },
-//   {
-//     nombre: "Roxana García",
-//     nota: 8,
-//   },
-//   {
-//     nombre: "Luciano Lopez",
-//     nota: 5,
-//   },
-//   {
-//     nombre: "Fernanda Gimenez",
-//     nota: 6,
-//   },
-//   {
-//     nombre: "Florencia Martinez",
-//     nota: 10,
-//   },
-//   {
-//     nombre: "Raul Sanchez",
-//     nota: 7,
-//   },
-//   {
-//     nombre: "Sandra Figueroa",
-//     nota: 8,
-//   },
-// ];
+let alumnos = [
+  {
+    nombre: "Juan Gomez",
+    nota: 7,
+  },
+  {
+    nombre: "Pedro Rodriguez",
+    nota: 4,
+  },
+  {
+    nombre: "Roxana García",
+    nota: 8,
+  },
+  {
+    nombre: "Luciano Lopez",
+    nota: 5,
+  },
+  {
+    nombre: "Fernanda Gimenez",
+    nota: 6,
+  },
+  {
+    nombre: "Florencia Martinez",
+    nota: 10,
+  },
+  {
+    nombre: "Raul Sanchez",
+    nota: 7,
+  },
+  {
+    nombre: "Sandra Figueroa",
+    nota: 8,
+  },
+];
 
-// let aprobados = alumnos.filter((alumno)=>{
-//   return  alumno.nota >=7
-// })
+let aprobados = alumnos.filter((alumno)=>{
+  return  alumno.nota >=7
+})
 
-// console.log(aprobados)
+console.log(aprobados)
