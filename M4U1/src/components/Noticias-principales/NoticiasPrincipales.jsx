ef49e4e0390f1af5459a1450391b2fe55e0895ea
facundomaksud@gmail.com <facundomaksud@gmail.com>
2023-06-06 15:29:36 -0300

    seccion noticias principales

--- M4U1/src/components/Noticias-principales/NoticiasPrincipales.jsx
+++ M4U1/src/components/Noticias-principales/NoticiasPrincipales.jsx
@@ -9,9 +9,9 @@ export const NoticiasPrincipales = () => {
           Noticias Principales<span>VER TODAS LAS NOTICIAS</span>
         </p>
       </div>
-      <div>
+      <div className={styles.noticiasContainer}>
         <div className={styles.noticiaCard}>
-          <img src={img1} alt="venta-de-entradas" />
+          <img src={img1} alt="venta-de-entradas" className={styles.imageNoticia}/>
           <h3 className={styles.noticiaTitle}>
             Venta de entradas - River vs. Fluminense {`"`}CONMEBOL Libertadores
             2023{`"`}
@@ -19,7 +19,7 @@ export const NoticiasPrincipales = () => {
         </div>
         <div className={styles.separador}></div>
         <div className={styles.noticiaCard}>
-          <img src={img2} alt="semillero-river" />
+          <img src={img2} alt="semillero-river" className={styles.imageNoticia}/>
           <h3 className={styles.noticiaTitle}>El semillero de River, presente en las finales europeas</h3>
         </div>
       </div>
