d1f408227b957c15dd38aa53dc4b872ddddb7299
facundomaksud@gmail.com <facundomaksud@gmail.com>
2023-06-16 17:38:01 -0300

    estilos responsive agregados

--- M4U2/client/src/components/Header/Header.jsx
+++ M4U2/client/src/components/Header/Header.jsx
@@ -4,27 +4,39 @@ import Navbar from "react-bootstrap/Navbar";
 import styles from "./Header.module.css";
 export const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="lg" className="p-0">
       <Container fluid className={styles.containerNavbar}>
         <Navbar.Brand href="#">
           <img
             src="https://static.wixstatic.com/media/94dfda_44df25021a40444f9ecd9f360dca28ca~mv2.png/v1/fill/w_164,h_39,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/homeslice%20png.png"
             alt="logo-pizzeria"
+            className={styles.logo}
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll" >
-          <Nav
-            className={`${"me-auto my-2 my-lg-0"} ${styles.menu}`}
-            style={{ maxHeight: "300px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1" className={styles.option}>INICIO</Nav.Link>
-            <Nav.Link href="#action2">NOSOTROS</Nav.Link>
-            <Nav.Link href="#action1">PRODUCTOS</Nav.Link>
-            <Nav.Link href="#action2">PIDE ONLINE</Nav.Link>
-            <Nav.Link href="#">RESERVA ONLINE</Nav.Link>
-            <Nav.Link href="#action1">EVENTOS</Nav.Link>
+        <Navbar.Toggle
+          aria-controls="navbarScroll"
+          className={styles.menuResponsive}
+        />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className={`${"me-auto my-2 my-lg-0"} ${styles.menu}`}>
+            <Nav.Link href="#action1" className={styles.option}>
+              INICIO
+            </Nav.Link>
+            <Nav.Link href="#action2" className={styles.option}>
+              NOSOTROS
+            </Nav.Link>
+            <Nav.Link href="#action1" className={styles.option}>
+              PRODUCTOS
+            </Nav.Link>
+            <Nav.Link href="#action2" className={styles.option}>
+              PIDE ONLINE
+            </Nav.Link>
+            <Nav.Link href="#" className={styles.option}>
+              RESERVA ONLINE
+            </Nav.Link>
+            <Nav.Link href="#action1" className={styles.option}>
+              EVENTOS
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
