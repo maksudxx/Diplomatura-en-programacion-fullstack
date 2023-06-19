bf25827bca25cd3458052ba57e44dee45fb46ce1
facundomaksud@gmail.com <facundomaksud@gmail.com>
2023-06-19 20:58:01 -0300

    lista de productos

---
+++ M4U2/client/src/utils/ListaProductos.js
@@ -0,0 +1,61 @@
+const lista = [
+  {
+    title: "PIZZA DE ALBAHACA Y CHERRY",
+    description:
+      "Una pizza muy sencilla de preparar y que lo único que requiere son un buen queso mozzarella, unos tomatitos cherry, una buena salsa de tomate casera y unas hojas de albahaca, serán suficientes para hacer esta deliciosa pizza.",
+    image:
+      "https://recetinas.com/wp-content/uploads/2017/09/pizza-de-albahaca-y-tomatitos-cherry-receta.jpg.webp",
+  },
+  {
+    title: "PIZZA 4 QUESOS",
+    description:
+      "Todo un clásico para los más queseros!! Una combinación de queso Fontina, queso Mozzarela, queso Parmigiano Reggiano y queso Gorgonzola que hará las delicias de todos los comensales.",
+    image:
+      "https://recetinas.com/wp-content/uploads/2018/04/pizza-cuatro-quesos.jpg.webp",
+  },
+  {
+    title: "PIZZA DE CHAMPIÑONES Y PEPERONI",
+    description:
+      "Si te gusta el pepperoni esta es tu pizza. Una combinación de este delicioso ingrediente con champiñones, tomate, queso y la mejor masa de pizza que te puedas imaginar. Todo un placer para nuestro paladar.",
+    image:
+      "https://recetinas.com/wp-content/uploads/2017/09/pizza-con-champinones-y-pepperoni.jpg.webp",
+  },
+  {
+    title: "PIZZA DE CALABACIN",
+    description:
+      "Esta pizza de calabacín quede súper cremosa como si de una quiche se tratase, todo ello gracias a la mezcla especial de nata, huevo y queso rallado que utilizaremos. Sin duda, esta es una opción muy recomendable que sorprenderá a todos los comensales.",
+    image:
+      "https://recetinas.com/wp-content/uploads/2019/10/pizza-de-calabacin-receta.jpg.webp",
+  },
+  {
+    title: "HAMBURGUESA DE CARNE PICADA",
+    description:
+      "Son las clásicas hamburguesas pero hechas en nuestra cocina, contiene un medallon de 200gr de carne picada a cuchillo el cual le da un excelente y unico sabor, ademas incluye lechuga, tomate y queso dambo",
+    image:
+      "https://recetinas.com/wp-content/uploads/2018/08/hamburguesas-de-carne-picada.jpg.webp",
+  },
+  {
+    title: "SMASH BURGUER",
+    description:
+      "La Smash Burger es un tipo de hamburguesa americana que últimamente se ha puesto muy de moda en prácticamente todo el mundo. Se diferencia de las hamburguesas clásicas en la técnica que se utiliza durante la cocción, que no es más que aplastarla con una espátula mientras se hace en una sartén o plancha a fuego fuerte. los ingredientes son los mismos que la hamburguesa clasica.",
+
+    image:
+      "https://recetinas.com/wp-content/uploads/2023/02/smash-burger.jpg.webp",
+  },
+  {
+    title: "HAMB. CON CECINA Y QUESO DE CABRA",
+    description:
+      "Deliciosa hamburguesa que tiene un medallon de carne de 200gr. junto con cecina, queso de cabra y plátano o banana. Una mezcla de sabores realmente impresionante!!",
+    image:
+      "https://recetinas.com/wp-content/uploads/2019/08/hamburguesa-con-cecina-y-queso-de-cabra.jpg.webp",
+  },
+  {
+    title: "HAMBURGUESA CON PAN NEGRO",
+    description:
+      "Hamburguesa clasica de Carne con una presentacion diferente: pan negro el cual resultarán muy llamativas y su sabor será igualmente sabroso.",
+    image:
+      "https://recetinas.com/wp-content/uploads/2023/03/Hamburguesa-con-pan-negro.jpg.webp",
+  },
+];
+
+export default lista;
