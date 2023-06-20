70f564aefe2b24e39e4953ffb9594c9bcfd640ce
facundomaksud@gmail.com <facundomaksud@gmail.com>
2023-06-19 22:18:07 -0300

    estilos componente grid productos

--- M4U2/client/src/App.jsx
+++ M4U2/client/src/App.jsx
@@ -1,17 +1,14 @@
-
-import './App.css'
-import { Header } from './components/Header/Header'
-import { Inicio } from './pages/Inicio/Inicio'
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { Inicio } from "./pages/Inicio/Inicio";
 
 function App() {
-  
-
   return (
     <>
-      <Header/>
-      <Inicio/>
+      <Header />
+      <Inicio />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- M4U2/client/src/components/GridProductos/GridProductos.jsx
+++ M4U2/client/src/components/GridProductos/GridProductos.jsx
@@ -1,13 +1,21 @@
-import listaProductos from '../../utils/ListaProductos'
-import CardProductos from '../CardProductos/CardProductos'
+import listaProductos from "../../utils/ListaProductos";
+import CardProductos from "../CardProductos/CardProductos";
 
+import styles from "./GridProductos.module.css";
 export const GridProductos = () => {
- 
   return (
-    <>
-      {listaProductos.map((product, index)=>(
-        <CardProductos key={index} title={product.title} description={product.description} image={product.image}/>
-      ))}
-    </>
-  )
-}
+    <div className={styles.productsContainer}>
+      <h1 className={styles.productsTitle}>RECOMENDACIONES DE LA CASA</h1>
+      <div>
+        {listaProductos.map((product, index) => (
+          <CardProductos
+            key={index}
+            title={product.title}
+            description={product.description}
+            image={product.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
