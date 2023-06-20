8f41c04fc3ec295411da72eb0d6592f40ec9c210
facundomaksud@gmail.com <facundomaksud@gmail.com>
2023-06-20 00:12:16 -0300

    margin boton ver mas productos

--- M4U2/client/src/components/ButtonProductos/ButtonProductos.jsx
+++ M4U2/client/src/components/ButtonProductos/ButtonProductos.jsx
@@ -1,9 +1,9 @@
 import Button from "react-bootstrap/Button";
-
+import styles from './ButtonProductos.module.css'
 export const ButtonProductos = ({ text }) => {
   return (
     <>
-      <Button variant="outline-warning">{text}</Button>{" "}
+      <Button variant="outline-warning" className={styles.boton}>{text}</Button>{" "}
     </>
   );
 };
