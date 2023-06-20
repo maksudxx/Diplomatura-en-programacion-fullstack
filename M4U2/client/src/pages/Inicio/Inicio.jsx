100e4824593acb73952ec2873686af58c66d858c
facundomaksud@gmail.com <facundomaksud@gmail.com>
2023-06-20 00:08:55 -0300

    estilos pagina inicio

--- M4U2/client/src/pages/Inicio/Inicio.jsx
+++ M4U2/client/src/pages/Inicio/Inicio.jsx
@@ -1,13 +1,13 @@
 import { ButtonProductos } from "../../components/ButtonProductos/ButtonProductos";
 import { GridProductos } from "../../components/GridProductos/GridProductos";
 import { ImgInicio } from "../../components/ImgInicio/ImgInicio";
-
+import styles from './Inicio.module.css'
 export const Inicio = () => {
   return (
-    <>
+    <div className={styles.container}>
       <ImgInicio />
       <GridProductos />
       <ButtonProductos text="VER MAS PRODUCTOS"/>
-    </>
+    </div>
   );
 };
