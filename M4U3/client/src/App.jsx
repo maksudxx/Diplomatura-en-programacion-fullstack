2b4ca66daf0d74d852244a6eccda77cf25eeb3e7
facundomaksud@gmail.com <facundomaksud@gmail.com>
2023-06-20 02:13:16 -0300

    pagina nosotros creada

--- M4U3/client/src/App.jsx
+++ M4U3/client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Inicio } from "./pages/Inicio/Inicio";
+import { Nosotros } from "./pages/Nosotros";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Inicio />
       <Footer/>
+      <Nosotros/>
     </>
   );
 }
--- M4U3/client/src/pages/Nosotros.jsx
+++ M4U3/client/src/pages/Nosotros.jsx
@@ -1,11 +1,38 @@
-import Container from 'react-bootstrap/Container'
-
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import styles from "./Nosotros.module.css";
 export const Nosotros = () => {
   return (
-    <>
-        <Container>
-            
-        </Container>
-    </>
-  )
-}
+    <Container fluid className={styles.nosotrosContainer}>
+      <div>
+        <h1 className={styles.titulo}>Desde 2022</h1>
+        <p className={styles.descripcion}>
+          Negocio que empezo como un emprendimiento familiar y que termino
+          siendo una de las franquicias mas grandes de la zona. trabajamos con
+          productos 100% seleccionados ofreciendole la mejor experiencia al
+          cliente tanto en atencion como en sabor.
+        </p>
+      </div>
+      <div className={styles.formulario}>
+        <h2 className={styles.tituloForm}>Trabaja con nosotros</h2>
+        <label htmlFor="">Nombre: </label>
+        <input type="text" name="nombre" placeholder="su nombre..." />
+        <label htmlFor="">Email: </label>
+        <input type="email" name="email" placeholder="email@ejemplo.com" />
+        <label htmlFor="">Celular: </label>
+        <input type="number" name="celular" placeholder="+9999999999" />
+        <label htmlFor="">Mensaje:</label>
+        <textarea
+          name="mensaje"
+          id=""
+          cols="40"
+          rows="2"
+          placeholder="dejar su mensaje aqui..."
+        ></textarea>
+        <Button variant="warning" className={styles.buttonEnviar}>
+          Enviar
+        </Button>
+      </div>
+    </Container>
+  );
+};
