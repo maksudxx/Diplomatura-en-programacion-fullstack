dafd27abf76ee75d6ac86d0b073fbf37665c0f89
facundomaksud@gmail.com <facundomaksud@gmail.com>
2023-06-24 16:56:26 -0300

    cambio de estilos en pagina 'nosotros'

--- PROYECTO_FINAL/client/src/pages/Nosotros/Nosotros.jsx
+++ PROYECTO_FINAL/client/src/pages/Nosotros/Nosotros.jsx
@@ -4,16 +4,17 @@ import styles from "./Nosotros.module.css";
 export const Nosotros = () => {
   return (
     <Container fluid className={styles.nosotrosContainer}>
-      <div>
-        <h1 className={styles.titulo}>Desde 2022</h1>
+      <div className={styles.info}>
+       <div>
+       <h1 className={styles.titulo}>Desde 2022</h1>
         <p className={styles.descripcion}>
           Negocio que empezo como un emprendimiento familiar y que termino
           siendo una de las franquicias mas grandes de la zona. trabajamos con
           productos 100% seleccionados ofreciendole la mejor experiencia al
           cliente tanto en atencion como en sabor.
         </p>
-      </div>
-      <div className={styles.formulario}>
+       </div>
+       <div className={styles.formulario}>
         <h2 className={styles.tituloForm}>Trabaja con nosotros</h2>
         <label htmlFor="">Nombre: </label>
         <input type="text" name="nombre" placeholder="su nombre..." />
@@ -28,11 +29,14 @@ export const Nosotros = () => {
           cols="40"
           rows="2"
           placeholder="dejar su mensaje aqui..."
+          className={styles.comments}
         ></textarea>
         <Button variant="warning" className={styles.buttonEnviar}>
           Enviar
         </Button>
       </div>
+      </div>
+      
     </Container>
   );
 };
